2e12185d6df836bddf0954d0d3d27f3f9223dc7c
Ronnie Smith <ronniesmith@outlook.com>
2021-12-07 16:30:18 +0000

    Update start.sh

--- hsr-docker-client-example/start.sh
+++ hsr-docker-client-example/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "192.168.1.211 hsrc" >> /etc/hosts
 export ROS_MASTER_URI=http://192.168.1.211:11311/
-export ROS_IP=192.168.1.224
+export ROS_IP=192.168.1.178
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
 rosrun hsr_head_positioner hsr_head_positioner.py
