280477c908e0839bb4cbb5e790792d4c09d9fe5a
Santosh Thoduka <santosh.thoduka@gmail.com>
2021-12-15 10:27:36 +0100

    change ip address

--- hsr-docker-client-example/start.sh
+++ hsr-docker-client-example/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "192.168.1.211 hsrc" >> /etc/hosts
-export ROS_MASTER_URI=http://192.168.1.211:11311/
-export ROS_IP=192.168.1.178
+echo "192.168.2.159 hsrc" >> /etc/hosts
+export ROS_MASTER_URI=http://192.168.2.159:11311/
+export ROS_IP=192.168.2.148
 . /opt/ros/melodic/setup.sh && cd /home/root/catkin_ws && catkin_make
 source /home/root/catkin_ws/devel/setup.bash
 roslaunch pointcloud_example_ros_ws detect_plane.launch
